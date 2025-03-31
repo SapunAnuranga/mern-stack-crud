2fc624cad159799f3ae9099e84ea5477033f49e8
SAPUN ANURANGA <sapunanuranga98@gmail.com>
2025-03-31 19:07:11 +0530

    Added delete user API.

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -41,7 +41,7 @@ export const getAllUsers = async (req, res) => {
       res.status(500).json({ errorMessage: error.message });
     }
   };
-  
+
   export const update = async (req, res) => {
     try {
       const id = req.params.id;
@@ -54,6 +54,20 @@ export const getAllUsers = async (req, res) => {
       });
       
       res.status(200).json({ message: "User Updated successfully." });
+    } catch (error) {
+      res.status(500).json({ errorMessage: error.message });
+    }
+  };
+
+  export const deleteUser = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const userExist = await User.findById(id);
+      if (!userExist) {
+        return res.status(404).json({ message: "User not found." });
+      }
+      await User.findByIdAndDelete(id);
+      res.status(200).json({ message: "User deleted successfully." });
     } catch (error) {
       res.status(500).json({ errorMessage: error.message });
     }
