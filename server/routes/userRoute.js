ed9fa3cfc6ae427fac0154f9eea61831ea711ec3
SAPUN ANURANGA <sapunanuranga98@gmail.com>
2025-03-31 19:04:50 +0530

    Added update user API.

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,7 +4,7 @@ import {
   create,
   getAllUsers,
   getUserById,
-
+  update,
 } from "../controller/userController.js";
 
 const route = express.Router();
@@ -12,5 +12,6 @@ const route = express.Router();
 route.post("/user", create);
 route.get("/users", getAllUsers);
 route.get("/user/:id", getUserById);
+route.put("/update/user/:id", update);
 
 export default route;
