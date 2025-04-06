36e16de754a0c2dfbc42b00e1e0e1bfb9a59c8dc
SAPUN ANURANGA <sapunanuranga98@gmail.com>
2025-04-06 09:53:38 +0530

    Added form submission logic with Axios and state handling.

--- client/src/AddUser/AddUser.jsx
+++ client/src/AddUser/AddUser.jsx
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AddUser.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const AddUser = () => {
+    const users = {
+      name: "",
+      email: "",
+      address: "",
+    };
+    const [user, setUser] = useState(users);
+    const navigate = useNavigate();
+  
+    const inputHandler = (e) => {
+      const { name, value } = e.target;
+      console.log(name, value);
+  
+      setUser({ ...user, [name]: value });
+    };
+  
+    const submitForm = async (e) => {
+      e.preventDefault();
+      await axios
+        .post("http://localhost:8000/api/user", user)
+        .then((response) => {
+          toast.success(response.data.message, { position: "top-right" });
+          navigate("/");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
     
-
     return (
         <div className='AddUser'>
             <Link to="/" className="btn btn-secondary">
                 <i className="fa-solid fa-backward"></i> Back 
             </Link>
             <h4>Add New User</h4>
-            <form className="AddUserForm">
+            <form className="AddUserForm" onSubmit={submitForm}>
                 <div className='inputGroup'>
                     <label htmlFor='name'>Name : </label>
                     <input
                         type='text'
                         id='name'
+                        onChange={inputHandler}
                         name='name'
+                        value={user.name}
                         autoComplete='off'
                         placeholder='Enter Your Name'
                     />
@@ -27,7 +57,9 @@ const AddUser = () => {
                     <input
                         type='email'
                         id='email'
+                        onChange={inputHandler}
                         name='email'
+                        value={user.email}
                         autoComplete='off'
                         placeholder='Enter Your Email'
                     />
@@ -37,13 +69,15 @@ const AddUser = () => {
                     <input
                         type='text'
                         id='address'
+                        onChange={inputHandler}
                         name='address'
+                        value={user.address}
                         autoComplete='off'
                         placeholder='Enter Your Address'
                     />
                 </div>
                 <div className='inputGroup'>
-                    <button type="button" className="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary">Submit</button>
                 </div>
             </form>
         </div>
