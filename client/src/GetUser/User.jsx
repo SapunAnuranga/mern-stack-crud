b9e94932bbd9b1f8efae5d62c413e0b9970bc418
SAPUN ANURANGA <sapunanuranga98@gmail.com>
2025-04-06 11:00:29 +0530

    Added deleteUser function to handle user deletion via API.

--- client/src/GetUser/User.jsx
+++ client/src/GetUser/User.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import "./user.css"
 import axios from "axios"
 import { Link } from 'react-router-dom'
+import toast from 'react-hot-toast'
 
 const User = () => {
     const [users, setUsers] = useState([])
@@ -17,43 +18,62 @@ const User = () => {
         }
 
         fetchData()
-    }, [])
+    }, []);
+
+    const deleteUser = async (userId) => {
+        await axios
+          .delete(`http://localhost:8000/api/delete/user/${userId}`)
+          .then((response) => {
+            setUsers((prevUser) => prevUser.filter((user) => user._id !== userId));
+            toast.success(response.data.message, { position: "top-right" });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      };
 
     return (
         <div className="userTable">
             <Link to ="/add" type="button" className="btn btn-primary">
-            Add User <i className="fa-solid fa-user-plus"></i>
+                Add User <i className="fa-solid fa-user-plus"></i>
             </Link>
 
-            <table className="table table-bordered">
-                <thead>
-                    <tr>
-                        <th scope="col">S.No</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Address</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user, index) => (
-                        <tr key={user.id || index}>
-                            <td>{index + 1}</td>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td>{user.address}</td>
-                            <td className='actionButton'>
-                                <Link to={`/update/${user._id}`} className="btn btn-info">
-                                     <i className="fa-solid fa-pen-to-square"></i>
-                                </Link>
-                                <button type="button" className="btn btn-danger">
-                                    <i className="fa-solid fa-trash"></i>
-                                </button>  
-                            </td>
+            {users.length === 0 ? (
+                <div className="no-data">
+                    <p>No data available. Please add new data.</p>
+                </div>
+            ) : (
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">S.No</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Address</th>
+                            <th scope="col">Actions</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {users.map((user, index) => (
+                            <tr key={user.id || index}>
+                                <td>{index + 1}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.address}</td>
+                                <td className='actionButton'>
+                                    <Link to={`/update/${user._id}`} className="btn btn-info">
+                                        <i className="fa-solid fa-pen-to-square"></i>
+                                    </Link>
+                                    <button onClick={() => deleteUser(user._id)}
+                                    type="button" className="btn btn-danger">
+                                        <i className="fa-solid fa-trash"></i>
+                                    </button>  
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     )
 }
